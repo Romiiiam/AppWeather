28e217aa794d3446e2b758ae055f42e547bf7143
elsac <carto.elsa@gmail.com>
2019-04-03 21:33:05 +0200

    update weather

--- components/Weather.js
+++ components/Weather.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, ImageBackground, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, TextInput, Button, Alert } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import PropTypes from 'prop-types';
 import { weatherConditions } from '../utils/WeatherConditions';
@@ -8,7 +8,31 @@ import { weatherConditions } from '../utils/WeatherConditions';
 export default class Weather extends React.Component {
   constructor(props) {
     super(props);
-    console.log(this.props);
+    this.state = {
+      value: '',
+      temperature: this.props.temperature,
+      weather: this.props.weather,
+      town: this.props.town
+    };
+    console.log(this.state);
+  }
+
+  _onPressButton() {
+    fetch(
+      `https://openweathermap.org/data/2.5/find?q=${this.state.value}&type=like&sort=population&cnt=30&appid=b6907d289e10d714a6e88b30761fae22&_=1554296103692`
+    )
+      .then(res => res.json())
+      .then(json => {
+        console.log("--------------------");
+        let new_val = Math.round(json.list[0].main.temp - 273.15) ;
+        this.setState({
+          temperature: new_val,
+          weather: json.list[0].weather[0].main,
+          town: json.list[0].name,
+          isLoading: false
+        });
+        console.log(this.state);
+      });
   }
 
 render(){
@@ -16,19 +40,21 @@ render(){
     <View
       style={[
         styles.weatherContainer,
-        { backgroundColor: weatherConditions[this.props.weather].color }
+        { backgroundColor: weatherConditions[this.state.weather].color }
       ]}
     >
       <ImageBackground
         style={{width: '100%', height: '100%'}}
-        source={{uri: weatherConditions[this.props.weather].img}}>
+        source={{uri: weatherConditions[this.state.weather].img}}>
       <View style={{marginTop: 80 ,flex: 1,flexDirection: 'row',alignItems: 'center',justifyContent: 'space-around'}}>
         <Text>Search by location </Text>
         <TextInput
           style={{width : 200,height:50, borderColor: 'gray', borderWidth: 1, borderRadius: 10,backgroundColor: 'white'}}
-          value=""
+          onChangeText={(value) => this.setState({value})}
+          value={this.state.value}
         />
         <Button
+          onPress={() => this._onPressButton()}
           title="go"
           color="#841584"
         />
@@ -36,16 +62,16 @@ render(){
       <View style={styles.headerContainer}>
         <MaterialCommunityIcons
           size={72}
-          name={weatherConditions[this.props.weather].icon}
+          name={weatherConditions[this.state.weather].icon}
           color={'#fff'}
         />
-        <Text style={styles.tempText}>{this.props.temperature}˚</Text>
+        <Text style={styles.tempText}>{this.state.temperature}˚</Text>
       </View>
       <View style={styles.centerContainer}>
-        <Text style={styles.title_town}>{this.props.town}</Text>
+        <Text style={styles.title_town}>{this.state.town}</Text>
       </View>
       <View style={styles.bodyContainer}>
-        <Text style={styles.title}>{weatherConditions[this.props.weather].title}</Text>
+        <Text style={styles.title}>{weatherConditions[this.state.weather].title}</Text>
       </View>
       </ImageBackground>
     </View>
